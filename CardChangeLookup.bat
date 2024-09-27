8820467bafb788adac931f326e00047d3abecee5
vghost01 <39235229+vghost01@users.noreply.github.com>
2024-09-27 21:27:01 +0300

    Adjust for missing requirements module

--- CardChangeLookup.bat
+++ CardChangeLookup.bat
@@ -1,4 +1,10 @@
 @echo off
+python -c "import requests" 2>nul
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Installing requirements...
+    pip install -r requirements.txt
+)
 python src/process.py
 set /p DUMMY=Press Enter to continue...
 @echo on
